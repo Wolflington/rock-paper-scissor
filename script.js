5581cffdd6b455e874a871463660c3a9dc328f2b
Joseph Ely Tuyor <ely.tuyor@yahoo.com>
2023-05-05 08:22:37 +0800

    Make a function that allows the computer to choose
    Make a function that gets input from the user.
    Write conditional statements to determine the winner and loser and return the statements.

---
+++ script.js
@@ -0,0 +1,31 @@
+function getComputerChoice() {
+    let choice = ['rock', 'paper', 'scissor'];
+    let result = choice[Math.floor( Math.random()*choice.length )];
+}
+getComputerChoice();
+console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt('Please choose your weapon: ', '');
+    computerSelection = getComputerChoice();
+
+    if (playerSelection === computerSelection) {
+        return `It's a tie! You chose ${playerSelection}.`;
+    } 
+    else if (playerSelection === 'rock' && computerSelection === 'scissor') {
+        return `You win! Rock crushes scissor!`;
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You win! Paper covers rock!';
+    } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
+        return 'You win! Scissor cuts paper!';
+    } 
+    else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You lose. Paper covers rock.';
+    } else if (playerSelection === 'paper' && computerSelection === 'scissor') {
+        return 'You lose. Scissor cuts paper.';
+    } else if (playerSelection === 'scissor' && computerSelection === 'rock') {
+        return 'You lose. Rock crushes scissor.';
+    }
+    
+}
+playRound();
