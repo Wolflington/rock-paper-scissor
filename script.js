9423ef7ce8d16403716dd1dfe8fb23f2a10f48b9
Joseph Ely Tuyor <ely.tuyor@yahoo.com>
2023-05-09 09:11:22 +0800

    Add return statement when the user cancels the prompt or enter invalid input

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ function getComputerChoice() {
     let result = choice[Math.floor( Math.random()*choice.length )];
     return result;
 }
+//global variables
+let playerSelection;
+let computerSelection;
+let playerScore = 0;
+let computerScore = 0;
+
+game();
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = prompt('Please choose your weapon: ', '').toLowerCase();
@@ -12,18 +19,26 @@ function playRound(playerSelection, computerSelection) {
         return `It's a tie! You chose ${playerSelection}.`;
     } 
     else if (playerSelection === 'rock' && computerSelection === 'scissor') {
+        playerScore++;
         return `You win! Rock crushes scissor!`;
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore++;
         return 'You win! Paper covers rock!';
     } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
+        playerScore++;
         return 'You win! Scissor cuts paper!';
     } 
     else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        computerScore++;
         return 'You lose. Paper covers rock.';
     } else if (playerSelection === 'paper' && computerSelection === 'scissor') {
+        computerScore++;
         return 'You lose. Scissor cuts paper.';
     } else if (playerSelection === 'scissor' && computerSelection === 'rock') {
+        computerScore++;
         return 'You lose. Rock crushes scissor.';
+    } else {
+        return 'Enter a valid move!'
     }
     
 }
@@ -32,6 +47,4 @@ function game() {
     for (let i = 0; i < 5; i++) {
     console.log(playRound());
 }
-
 }
-game();
